48ef9d28530eca3d6c005ec1853afd8e6f5b3f58
joear <joearnold12345@hotmail.co.uk>
2024-05-05 14:43:12 +0100

    fixed recording sleep mode as network outage

--- BetterStayOnline2/ConnectionLoss/DownDetector.cs
+++ BetterStayOnline2/ConnectionLoss/DownDetector.cs
@@ -1,4 +1,5 @@
 ﻿using BetterStayOnline2.Charts;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,18 +15,48 @@ namespace BetterStayOnline2.ConnectionLoss
         {
             // Subscribe to the NetworkAvailabilityChanged event
             NetworkChange.NetworkAvailabilityChanged += OnNetworkAvailabilityChanged;
+            SystemEvents.PowerModeChanged += OnPowerModeChanged;
         }
+
+        private bool recordThisNextNetworkChange = true;
 
         // Event handler method for NetworkAvailabilityChanged event
         private void OnNetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
         {
             if (e.IsAvailable)
             {
-                PlotManager.NetworkConnectionEstablished();
+                if (recordThisNextNetworkChange)
+                {
+                    PlotManager.NetworkConnectionEstablished();
+                }
             }
             else
             {
-                PlotManager.NetworkConnectionLost();
+                // Don't record network losses when asleep
+                if (!isAsleep)
+                {
+                    recordThisNextNetworkChange = true;
+                    PlotManager.NetworkConnectionLost();
+                }
+                else
+                {
+                    recordThisNextNetworkChange = false;
+                }
+            }
+        }
+
+        private bool isAsleep = false;
+
+        // Event handler to listen for machine going to sleep
+        private void  OnPowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            if (e.Mode == PowerModes.Suspend)
+            {
+                isAsleep = true;
+            }
+            else if(e.Mode == PowerModes.Resume)
+            {
+                isAsleep = false;
             }
         }
     }
